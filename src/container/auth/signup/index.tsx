00cc873e243254892116114f12019d70a0fc29d9
etornam anyidoho <etornamanyidoho@outlook.com>
2024-02-27 14:53:18 +0000

    add auth

--- src/container/auth/signup/index.tsx
+++ src/container/auth/signup/index.tsx
@@ -6,8 +6,8 @@ import { Creators as AuthCreators } from '../../../services/redux/auth/actions'
 import { IAuthState, ISignUpRequest } from '../../../models/auth.model'
 import SignUpForm from './components/SignUpForm'
 import { ERole } from '../../../models/user.model'
-import { useNavigate } from 'react-router-dom'
-import logo from '../../../assets/img/signup.jpg'
+// import logo from '../../../assets/img/signup.jpg'
+
 
 const SignUpIndex: React.FC = () => {
   const {
@@ -23,14 +23,17 @@ const SignUpIndex: React.FC = () => {
     if(!posting && post_success) {
       form.resetFields()
     }
+    //eslint-disable-next-line
   }, [post_success, posting])
 
   return (
-    <Row style={{ height: '100vh', width: '100%' }}>
-      <Col md={12} lg={12} sm={0} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-        <img src={logo} alt='signup' style={{ width: '80%', height: '80%' }} />
+    <Row style={{ height: '100vh', width: '100%', display: 'flex', flexDirection: 'row' }}>
+      <Col md={16} lg={16} sm={0} xs={0} 
+        style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundImage: "url('/img/signup.jpg')", backgroundSize: '90% 90%', backgroundRepeat: 'no-repeat' }}
+      >
+        {/* <img src={logo} alt='signup' style={{ width: '100%', height: '100%' }} /> */}
       </Col>
-      <Col md={12} lg={12} sm={24} xs={24} style={{ boxShadow: '-2px 0 6px rgba(0, 0, 0, 0.09)' }}>
+      <Col md={8} lg={8} sm={24} xs={24} style={{ boxShadow: '-2px 0 6px rgba(0, 0, 0, 0.09)' }}>
         <Card style={{ padding: 30 }}>
           <Row>
             <Col span={24} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
